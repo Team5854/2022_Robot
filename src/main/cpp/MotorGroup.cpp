f1abd6dd837fe17fcd82cf92ef8bd07c54c09eef
bznerd <guyinthechair631@gmail.com>
2022-01-17 16:55:44 -0500

    Added Drivetrain class and made adjustments to MotorGroup

--- src/main/cpp/MotorGroup.cpp
+++ src/main/cpp/MotorGroup.cpp
@@ -2,17 +2,18 @@
 #include <vector>
 #include <initializer_list>
 
-class MotorGroup{
+class MotorGroup{// Group multiple talons for basic drive functionality
   public:
-    std::vector<TalonSRX*> talons;
-    MotorGroup(std::initializer_list<int> talonIds){
-      for (int id : talonIds) talons.push_back(new TalonSRX(id));
-      for (unsigned int i = 1; i < talons.size(); i++){
+    std::vector<TalonSRX*> talons;// Vector of pointers to talon objects
+    MotorGroup(std::initializer_list<int> talonIds){ 
+      for (int id : talonIds) talons.push_back(new TalonSRX(id));// Add a new talon object to the vector for each id given to the constructor
+      for (unsigned int i = 1; i < talons.size(); i++){// Set each talon after the first listed to follow the first talon
         talons[i]->Follow(*talons[0]);
       }
     }
-    Set(double speed){
-      if(speed <= -1 || speed >= 1){
+    void Set(double speed){// Set speed of the talons in basic PercentOutput control mode
+      if(speed < -1 || speed > 1){
+        std::cout<<"Invalid MotorGroup Set() parameter"<<endl;
         throw 1;
       }
       else{
